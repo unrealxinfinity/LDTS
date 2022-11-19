d0e6b88b58361d00e77380075bdadcc0083edf9b
Onso37 <80353484+Onso37@users.noreply.github.com>
2022-11-19 11:59:10 +0000

    Position class
    Just the bare functionality for now to allow further GUI development

---
+++ src/main/java/pt/up/fe/edu/hero/model/Position.java
@@ -0,0 +1,24 @@
+package pt.up.fe.edu.hero.model;
+
+public class Position {
+    private final int x;
+    private final int y;
+
+    public Position(int x, int y) {this.x = x; this.y = y;}
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return x == position.x && y == position.y;
+    }
+}
