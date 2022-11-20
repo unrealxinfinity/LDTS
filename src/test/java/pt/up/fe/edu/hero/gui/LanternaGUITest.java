4f430efb06d9ea118c076af08e6da4a3b5ceda69
Onso37 <80353484+Onso37@users.noreply.github.com>
2022-11-20 17:58:29 +0000

    added tests

--- src/test/java/pt/up/fe/edu/hero/gui/LanternaGUITest.java
+++ src/test/java/pt/up/fe/edu/hero/gui/LanternaGUITest.java
@@ -1,21 +1,32 @@
 package pt.up.fe.edu.hero.gui;
 
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.screen.Screen;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import pt.up.fe.edu.hero.model.Position;
 
 import java.io.IOException;
 
 public class LanternaGUITest {
-    private Screen setUpTest() {
-        return Mockito.mock(Screen.class);
+    private Screen screen;
+    private LanternaGUI gui;
+    private TextGraphics graphics;
+
+    @BeforeEach
+    void setUp() {
+        screen = Mockito.mock(Screen.class);
+        graphics = Mockito.mock(TextGraphics.class);
+
+        Mockito.when(screen.newTextGraphics()).thenReturn(graphics);
+
+        gui = new LanternaGUI(screen);
     }
 
     @Test
     public void closeTest() throws IOException {
-        Screen screen = setUpTest();
-        LanternaGUI gui = new LanternaGUI(screen);
-
         gui.close();
 
         Mockito.verify(screen, Mockito.times(1)).close();
@@ -23,9 +34,6 @@ public class LanternaGUITest {
 
     @Test
     public void refreshTest() throws IOException {
-        Screen screen = setUpTest();
-        LanternaGUI gui = new LanternaGUI(screen);
-
         gui.refresh();
 
         Mockito.verify(screen, Mockito.times(1)).refresh();
@@ -33,11 +41,48 @@ public class LanternaGUITest {
 
     @Test
     public void clearTest() throws IOException {
-        Screen screen = setUpTest();
-        LanternaGUI gui = new LanternaGUI(screen);
-
         gui.clear();
 
         Mockito.verify(screen, Mockito.times(1)).clear();
+    }
+
+    @Test
+    void drawDozer() {
+        gui.drawDozer(new Position(1, 1));
+
+        Mockito.verify(graphics, Mockito.times(1)).setForegroundColor(TextColor.Factory.fromString("#F3DF2B"));
+        Mockito.verify(graphics, Mockito.times(1)).putString(1, 2, "D");
+    }
+
+    @Test
+    void drawBoulderInTarget() {
+        gui.drawBoulder(new Position(1, 1), true);
+
+        Mockito.verify(graphics, Mockito.times(1)).setForegroundColor(TextColor.Factory.fromString("#D05E3B"));
+        Mockito.verify(graphics, Mockito.times(1)).putString(1, 2, "B");
+    }
+
+    @Test
+    void drawBoulderOffTarget() {
+        gui.drawBoulder(new Position(1, 1), false);
+
+        Mockito.verify(graphics, Mockito.times(1)).setForegroundColor(TextColor.Factory.fromString("#362F2D"));
+        Mockito.verify(graphics, Mockito.times(1)).putString(1, 2, "B");
+    }
+
+    @Test
+    void drawWall() {
+        gui.drawWall(new Position(1, 1));
+
+        Mockito.verify(graphics, Mockito.times(1)).setForegroundColor(TextColor.Factory.fromString("#E0E0E0"));
+        Mockito.verify(graphics, Mockito.times(1)).putString(1, 2, "#");
+    }
+
+    @Test
+    void drawTarget() {
+        gui.drawTarget(new Position(1, 1));
+
+        Mockito.verify(graphics, Mockito.times(1)).setForegroundColor(TextColor.Factory.fromString("#FF0000"));
+        Mockito.verify(graphics, Mockito.times(1)).putString(1, 2, "T");
     }
 }
