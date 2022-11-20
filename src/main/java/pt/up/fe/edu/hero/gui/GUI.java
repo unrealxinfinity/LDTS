c24e12b74af027b3fd7f1febc71ae3b48ee3962f
Onso37 <80353484+Onso37@users.noreply.github.com>
2022-11-20 17:41:22 +0000

    added methods to interface

--- src/main/java/pt/up/fe/edu/hero/gui/GUI.java
+++ src/main/java/pt/up/fe/edu/hero/gui/GUI.java
@@ -1,5 +1,7 @@
 package pt.up.fe.edu.hero.gui;
 
+import pt.up.fe.edu.hero.model.Position;
+
 import java.io.IOException;
 
 public interface GUI {
@@ -8,6 +10,16 @@ public interface GUI {
     void refresh() throws IOException;
 
     void close() throws IOException;
+
+    ACTION getNextAction() throws IOException;
+
+    void drawDozer(Position position);
+
+    void drawWall(Position position);
+
+    void drawTarget(Position position);
+
+    void drawBoulder(Position position, Boolean isInTarget);
 
     enum ACTION {UP, RIGHT, DOWN, LEFT, NONE, QUIT, PAUSE};
 }
