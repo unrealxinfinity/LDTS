48544d1558d9b1f5f024f7bdd6e6e73ef0ecfbea
Onso37 <80353484+Onso37@users.noreply.github.com>
2022-11-21 09:23:09 +0000

    added boulder and target

---
+++ src/main/java/pt/up/fe/edu/hero/model/game/elements/Boulder.java
@@ -0,0 +1,7 @@
+package pt.up.fe.edu.hero.model.game.elements;
+
+public class Boulder extends MovableElement{
+    public Boulder(int x, int y) {
+        super(x, y);
+    }
+}
---
+++ src/main/java/pt/up/fe/edu/hero/model/game/elements/Target.java
@@ -0,0 +1,7 @@
+package pt.up.fe.edu.hero.model.game.elements;
+
+public class Target extends Element{
+    public Target(int x, int y) {
+        super(x, y);
+    }
+}
