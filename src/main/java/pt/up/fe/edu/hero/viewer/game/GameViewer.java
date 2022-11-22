e22c0cecae5487968332d57b7d6157f864ab53c4
Onso37 <80353484+Onso37@users.noreply.github.com>
2022-11-22 09:01:37 +0000

    create viewers for game

---
+++ src/main/java/pt/up/fe/edu/hero/viewer/game/GameViewer.java
@@ -0,0 +1,29 @@
+package pt.up.fe.edu.hero.viewer.game;
+
+import pt.up.fe.edu.hero.gui.GUI;
+import pt.up.fe.edu.hero.model.game.arena.Arena;
+import pt.up.fe.edu.hero.model.game.elements.Element;
+import pt.up.fe.edu.hero.viewer.Viewer;
+
+import java.util.List;
+
+public class GameViewer extends Viewer<Arena> {
+    public GameViewer(Arena arena) {
+        super(arena);
+    }
+
+    @Override
+    public void drawElements(GUI gui) {
+        drawElements(gui);
+    }
+
+    private <T extends Element> void drawElements(GUI gui, List<T> elements, ElementViewer<T> viewer) {
+        for (T element : elements) {
+            drawElement(gui, element, viewer);
+        }
+    }
+
+    private <T extends Element> void drawElement(GUI gui, T element, ElementViewer<T> viewer) {
+        viewer.draw(element, gui);
+    }
+}
