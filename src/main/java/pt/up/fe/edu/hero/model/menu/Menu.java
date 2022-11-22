3233b1b65050ae082e2b2b54c5233f58ee846125
Onso37 <80353484+Onso37@users.noreply.github.com>
2022-11-22 15:09:11 +0000

    create abstract menu class

---
+++ src/main/java/pt/up/fe/edu/hero/model/menu/Menu.java
@@ -0,0 +1,36 @@
+package pt.up.fe.edu.hero.model.menu;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class Menu {
+    private final List<String> entries;
+    private int currentEntry = 0;
+
+    public Menu(String... strings) {
+        entries = Arrays.asList(strings);
+    }
+
+    public void nextEntry() {
+        currentEntry++;
+        if (currentEntry > this.entries.size() -1) currentEntry = 0;
+    }
+
+    public void previousEntry() {
+        currentEntry--;
+        if (currentEntry < 0) currentEntry = this.entries.size()-1;
+    }
+
+    public String getEntry(int i) {
+        return entries.get(i);
+    }
+
+    public boolean isSelected(int i) {
+        return currentEntry == i;
+    }
+
+    public int getNumberEntries() {
+        return this.entries.size();
+    }
+}
