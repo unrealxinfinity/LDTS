9d3677bc41d87e5cdec3cd52b559fb6b10edf7f9
Onso37 <80353484+Onso37@users.noreply.github.com>
2022-12-02 15:05:13 +0000

    create abstract builder

---
+++ src/main/java/pt/up/fe/edu/dozer/model/game/arena/ArenaBuilder.java
@@ -0,0 +1,30 @@
+package pt.up.fe.edu.dozer.model.game.arena;
+
+import pt.up.fe.edu.dozer.model.game.elements.Boulder;
+import pt.up.fe.edu.dozer.model.game.elements.Dozer;
+import pt.up.fe.edu.dozer.model.game.elements.Target;
+import pt.up.fe.edu.dozer.model.game.elements.Wall;
+
+import java.util.List;
+
+public abstract class ArenaBuilder {
+    public Arena createArena() {
+        Arena arena = new Arena(getWidth(), getHeight());
+
+        arena.setDozer(createDozer());
+        arena.setWalls(createWalls());
+        arena.setCollisionWalls(createCollisionWalls());
+        arena.setBoulders(createBoulders());
+        arena.setTargets(createTargets());
+
+        return arena;
+    }
+
+    protected abstract int getWidth();
+    protected abstract int getHeight();
+    protected abstract Dozer createDozer();
+    protected abstract List<Wall> createWalls();
+    protected abstract List<Wall> createCollisionWalls();
+    protected abstract List<Boulder> createBoulders();
+    protected abstract List<Target> createTargets();
+}
