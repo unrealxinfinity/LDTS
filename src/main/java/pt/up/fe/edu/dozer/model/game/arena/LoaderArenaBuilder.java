69fba18edb952ea17d95e4dc9912f506f5cde050
Onso37 <80353484+Onso37@users.noreply.github.com>
2022-12-02 15:10:35 +0000

    create file loader

---
+++ src/main/java/pt/up/fe/edu/dozer/model/game/arena/LoaderArenaBuilder.java
@@ -0,0 +1,102 @@
+package pt.up.fe.edu.dozer.model.game.arena;
+
+import pt.up.fe.edu.dozer.model.game.elements.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoaderArenaBuilder extends ArenaBuilder{
+    private final int level;
+    private final List<String> lines;
+
+    public LoaderArenaBuilder(int level) throws IOException {
+        this.level = level;
+
+        URL resource = LoaderArenaBuilder.class.getResource("/levels/level" + level + ".lvl");
+        BufferedReader br = new BufferedReader(new FileReader(resource.getFile()));
+
+        lines = readLines(br);
+    }
+
+    private List<String> readLines(BufferedReader br) throws IOException {
+        List<String> lines = new ArrayList<>();
+        for (String line; (line = br.readLine()) != null; )
+            lines.add(line);
+        return lines;
+    }
+
+    @Override
+    protected int getWidth() {
+        int width = 0;
+        for (String line : lines)
+            width = Math.max(width, line.length());
+        return width;
+    }
+
+    @Override
+    protected int getHeight() {
+        return lines.size();
+    }
+
+    @Override
+    protected List<Wall> createWalls() {
+        List<Wall> walls = new ArrayList<>();
+
+        for (int y = 0; y < lines.size(); y++) {
+            String line = lines.get(y);
+            for (int x = 0; x < line.length(); x++)
+                if (line.charAt(x) == '#') walls.add(new Wall(x, y));
+        }
+        return walls;
+    }
+
+    @Override
+    protected List<Boulder> createBoulders() {
+        List<Boulder> boulders = new ArrayList<>();
+
+        for (int y = 0; y < lines.size(); y++) {
+            String line = lines.get(y);
+            for (int x = 0; x < line.length(); x++)
+                if (line.charAt(x) == 'B') boulders.add(new Boulder(x, y));
+        }
+        return boulders;
+    }
+
+    @Override
+    protected Dozer createDozer() {
+        for (int y = 0; y < lines.size(); y++) {
+            String line = lines.get(y);
+            for (int x = 0; x < line.length(); x++)
+                if (line.charAt(x) == 'D') return new Dozer(x, y);
+        }
+        return null;
+    }
+
+    @Override
+    protected List<Wall> createCollisionWalls() {
+        List<Wall> walls = new ArrayList<>();
+
+        for (int y = 0; y < lines.size(); y++) {
+            String line = lines.get(y);
+            for (int x = 0; x < line.length(); x++)
+                if (line.charAt(x) == 'W') walls.add(new ImportantWall(x, y));
+        }
+        return walls;
+    }
+
+    @Override
+    protected List<Target> createTargets() {
+        List<Target> targets = new ArrayList<>();
+
+        for (int y = 0; y < lines.size(); y++) {
+            String line = lines.get(y);
+            for (int x = 0; x < line.length(); x++)
+                if (line.charAt(x) == 'W') targets.add(new Target(x, y));
+        }
+        return targets;
+    }
+}
