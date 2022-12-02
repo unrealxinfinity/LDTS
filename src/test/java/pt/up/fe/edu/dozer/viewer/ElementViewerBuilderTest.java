fd728ec95a42d17d5f64dd538d9e0dcb28ed7eda
Onso37 <80353484+Onso37@users.noreply.github.com>
2022-12-02 14:55:50 +0000

    killed elementviewerbuilder mutations

---
+++ src/test/java/pt/up/fe/edu/dozer/viewer/ElementViewerBuilderTest.java
@@ -0,0 +1,43 @@
+package pt.up.fe.edu.dozer.viewer;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pt.up.fe.edu.dozer.viewer.game.*;
+
+public class ElementViewerBuilderTest {
+    private ElementViewerBuilder builder;
+
+    @BeforeEach
+    public void setUp() {
+        this.builder = new ElementViewerBuilder();
+    }
+
+    @Test
+    public void getDozerViewer() {
+        DozerViewer viewer = builder.getDozerViewer();
+
+        Assertions.assertNotNull(viewer);
+    }
+
+    @Test
+    public void getWallViewer() {
+        WallViewer viewer = builder.getWallViewer();
+
+        Assertions.assertNotNull(viewer);
+    }
+
+    @Test
+    public void getBoulderViewer() {
+        BoulderViewer viewer = builder.getBoulderViewer();;
+
+        Assertions.assertNotNull(viewer);
+    }
+
+    @Test
+    public void getTargetViewer() {
+        TargetViewer viewer = builder.getTargetViewer();
+
+        Assertions.assertNotNull(viewer);
+    }
+}
