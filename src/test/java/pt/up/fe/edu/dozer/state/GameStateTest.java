28aea36e4d5dae076e0c5c89815e3dc0387aa3c8
Onso37 <80353484+Onso37@users.noreply.github.com>
2022-12-09 01:36:39 +0000

    state tests

--- src/test/java/pt/up/fe/edu/dozer/state/GameStateTest.java
+++ src/test/java/pt/up/fe/edu/dozer/state/GameStateTest.java
@@ -1,4 +1,43 @@
 package pt.up.fe.edu.dozer.state;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import pt.up.fe.edu.dozer.gui.GUI;
+import pt.up.fe.edu.dozer.model.game.arena.Arena;
+
+import java.io.IOException;
+
 public class GameStateTest {
+    @Test
+    public void getModel() {
+        GameState state = new GameState(new Arena(20, 20));
+
+        Assertions.assertNotNull(state.getModel());
+    }
+
+    @Test
+    public void getViewer() {
+        GameState state = new GameState(new Arena(20,20));
+
+        Assertions.assertNotNull(state.getViewer());
+    }
+
+    @Test
+    public void getController() {
+        GameState state = new GameState(new Arena(20,20));
+
+        Assertions.assertNotNull(state.getController());
+    }
+
+    @Test
+    public void stepTest() throws IOException {
+        GUI gui = Mockito.mock(GUI.class);
+        Mockito.when(gui.getNextAction()).thenReturn(GUI.ACTION.LEFT);
+        GameState state = new GameState(new Arena(20,20));
+
+        state.step(null, gui, 0);
+
+        Mockito.verify(gui, Mockito.times(1)).getNextAction();
+    }
 }
