ebaa3fb92e9567cc4c3120afe028bacc660afa4e
Onso37 <80353484+Onso37@users.noreply.github.com>
2022-12-09 03:06:16 +0000

    step tests for controller

--- src/test/java/pt/up/fe/edu/dozer/controller/editor/EditorControllersTest.java
+++ src/test/java/pt/up/fe/edu/dozer/controller/editor/EditorControllersTest.java
@@ -2,7 +2,13 @@ package pt.up.fe.edu.dozer.controller.editor;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import pt.up.fe.edu.dozer.MainGame;
+import pt.up.fe.edu.dozer.gui.GUI;
 import pt.up.fe.edu.dozer.model.game.arena.EditorArena;
+import pt.up.fe.edu.dozer.model.game.elements.Placer;
+
+import java.io.IOException;
 
 public class EditorControllersTest {
     @Test
@@ -31,5 +37,30 @@ public class EditorControllersTest {
         EditorArenaTargetController controller = new EditorArenaTargetController(new EditorArena(20,20));
 
         Assertions.assertNotNull(controller.cycleState());
+    }
+
+    @Test
+    public void stepCycle() throws IOException {
+        EditorArena arena = Mockito.mock(EditorArena.class);
+        MainGame game = Mockito.mock(MainGame.class);
+        EditorArenaDozerController controller = new EditorArenaDozerController(arena);
+
+        controller.step(game, GUI.ACTION.CYCLE, 0);
+
+        Mockito.verify(game, Mockito.times(1)).setState(Mockito.any());
+    }
+
+    @Test
+    public void stepSelect() throws IOException {
+        EditorArena arena = Mockito.mock(EditorArena.class);
+        MainGame game = Mockito.mock(MainGame.class);
+        Placer placer = Mockito.mock(Placer.class);
+        Mockito.when(arena.getPlacer()).thenReturn(placer);
+        EditorArenaDozerController controller = new EditorArenaDozerController(arena);
+
+        controller.step(game, GUI.ACTION.SELECT, 0);
+
+        Mockito.verify(arena, Mockito.times(2)).getPlacer();
+        Mockito.verify(placer, Mockito.times(2)).getPosition();
     }
 }
