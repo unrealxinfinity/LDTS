af05c75ca710d0184a91be1e548bf122394d8f46
HaoChang Fu <fuhaochang664961423@hotmail.com>
2022-12-10 18:55:47 +0000

    small changes to audio

--- src/main/java/pt/up/fe/edu/dozer/audio/AudioManager.java
+++ src/main/java/pt/up/fe/edu/dozer/audio/AudioManager.java
@@ -29,13 +29,14 @@ public class AudioManager {
     }
     public void restartAudio() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
         // implementacao mais custosa mas menos bug
-       /*URL resource= AudioManager.class.getResource(filepath);
+        //clip.close();
+        URL resource= AudioManager.class.getResource(filepath);
         soundStream= AudioSystem.getAudioInputStream(resource);
         DataLine.Info info = new DataLine.Info(Clip.class,soundStream.getFormat());
         clip=(Clip) AudioSystem.getLine(info);
-        clip.open(soundStream);*/
+        clip.open(soundStream);
         //implementacao menos custosa mas se clicar 2 vezes o botao quase ao mesmo tempo o som so da uma vez
-        clip.setMicrosecondPosition(0);
+        //clip.setFramePosition(0);
 
     }
     public void loop_sound(){
