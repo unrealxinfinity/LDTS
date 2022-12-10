711084d024ccb442354d61bc63f46a3af0205db0
Onso37 <80353484+Onso37@users.noreply.github.com>
2022-12-10 00:57:15 +0000

    create copy of arena

--- src/main/java/pt/up/fe/edu/dozer/model/game/arena/EditorArena.java
+++ src/main/java/pt/up/fe/edu/dozer/model/game/arena/EditorArena.java
@@ -1,7 +1,9 @@
 package pt.up.fe.edu.dozer.model.game.arena;
 
-import pt.up.fe.edu.dozer.model.game.elements.Dozer;
-import pt.up.fe.edu.dozer.model.game.elements.Placer;
+import pt.up.fe.edu.dozer.model.game.elements.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EditorArena extends Arena{
     public EditorArena(int width, int height) {
@@ -11,10 +13,23 @@ public class EditorArena extends Arena{
 
     public Arena getArena() {
         Arena arena = new Arena(getWidth(), getHeight());
-        arena.setDozer(getDozer());
-        arena.setCollisionWalls(getCollisionWalls());
-        arena.setTargets(getTargets());
-        arena.setBoulders(getBoulders());
+        arena.setDozer(new Dozer(getDozer().getPosition().getX(), getDozer().getPosition().getY()));
+
+        List<Wall> walls = new ArrayList<>();
+        for (Wall wall : getCollisionWalls())
+            walls.add(new ImportantWall(wall.getPosition().getX(), wall.getPosition().getY()));
+        arena.setCollisionWalls(walls);
+
+        List<Target> targets = new ArrayList<>();
+        for (Target target : getTargets())
+            targets.add(new Target(target.getPosition().getX(), target.getPosition().getY()));
+        arena.setTargets(targets);
+
+        List<Boulder> boulders = new ArrayList<>();
+        for (Boulder boulder : getBoulders())
+            boulders.add(new Boulder(boulder.getPosition().getX(), boulder.getPosition().getY()));
+        arena.setBoulders(boulders);
+        
         return arena;
     }
 
