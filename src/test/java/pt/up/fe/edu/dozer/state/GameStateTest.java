f56c55a409818ffa388a71bb47e92d5dbcb15a49
Onso37 <80353484+Onso37@users.noreply.github.com>
2022-12-10 11:41:30 +0000

    adjust a test

--- src/test/java/pt/up/fe/edu/dozer/state/GameStateTest.java
+++ src/test/java/pt/up/fe/edu/dozer/state/GameStateTest.java
@@ -3,6 +3,7 @@ package pt.up.fe.edu.dozer.state;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import pt.up.fe.edu.dozer.MainGame;
 import pt.up.fe.edu.dozer.gui.GUI;
 import pt.up.fe.edu.dozer.model.game.arena.Arena;
 
@@ -33,11 +34,13 @@ public class GameStateTest {
     @Test
     public void stepTest() throws IOException {
         GUI gui = Mockito.mock(GUI.class);
+        MainGame game = Mockito.mock(MainGame.class);
         Mockito.when(gui.getNextAction()).thenReturn(GUI.ACTION.LEFT);
         GameState state = new GameState(new Arena(20,20));
 
-        state.step(null, gui, 0);
+        state.step(game, gui, 0);
 
         Mockito.verify(gui, Mockito.times(1)).getNextAction();
+        Mockito.verify(game, Mockito.times(1)).resetTimer();
     }
 }
