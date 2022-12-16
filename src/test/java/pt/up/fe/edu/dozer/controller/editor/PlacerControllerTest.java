07174ddf41cc0900c8d3aa308b4b9aae28c2c2fc
Onso37 <80353484+Onso37@users.noreply.github.com>
2022-12-16 14:02:53 +0000

    added placer controller tests

---
+++ src/test/java/pt/up/fe/edu/dozer/controller/editor/PlacerControllerTest.java
@@ -0,0 +1,56 @@
+package pt.up.fe.edu.dozer.controller.editor;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import pt.up.fe.edu.dozer.gui.GUI;
+import pt.up.fe.edu.dozer.model.Position;
+import pt.up.fe.edu.dozer.model.game.arena.EditorArena;
+import pt.up.fe.edu.dozer.model.game.elements.Placer;
+
+import java.io.IOException;
+
+public class PlacerControllerTest {
+    private EditorArena arena;
+    private PlacerController controller;
+    private Placer placer;
+
+    @BeforeEach
+    public void setUp() {
+        arena = Mockito.mock(EditorArena.class);
+        Mockito.when(arena.getWidth()).thenReturn(15);
+        Mockito.when(arena.getHeight()).thenReturn(50);
+        placer = Mockito.mock(Placer.class);
+        Mockito.when(placer.getPosition()).thenReturn(new Position(14, 0));
+        Mockito.when(arena.getPlacer()).thenReturn(placer);
+        controller = new PlacerController(arena);
+    }
+
+    @Test
+    public void moveUp() throws IOException {
+        controller.step(null, GUI.ACTION.UP, 0);
+
+        Mockito.verify(placer, Mockito.times(0)).setPosition(Mockito.any());
+    }
+
+    @Test
+    public void moveLeft() throws IOException {
+        controller.step(null, GUI.ACTION.LEFT, 0);
+
+        Mockito.verify(placer, Mockito.times(1)).setPosition(Mockito.eq(new Position(13, 0)));
+    }
+
+    @Test
+    public void moveRight() throws IOException {
+        controller.step(null, GUI.ACTION.RIGHT, 0);
+
+        Mockito.verify(placer, Mockito.times(0)).setPosition(Mockito.any());
+    }
+
+    @Test
+    public void moveDown() throws IOException {
+        controller.step(null, GUI.ACTION.DOWN, 0);
+
+        Mockito.verify(placer, Mockito.times(1)).setPosition(Mockito.eq(new Position(14, 1)));
+    }
+}
