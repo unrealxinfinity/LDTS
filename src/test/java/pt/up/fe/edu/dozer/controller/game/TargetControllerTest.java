49be19c6d44cd63cf79213503d84f36403647d4c
Onso37 <80353484+Onso37@users.noreply.github.com>
2022-12-16 14:08:20 +0000

    added target controller test

---
+++ src/test/java/pt/up/fe/edu/dozer/controller/game/TargetControllerTest.java
@@ -0,0 +1,24 @@
+package pt.up.fe.edu.dozer.controller.game;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import pt.up.fe.edu.dozer.model.game.arena.Arena;
+import pt.up.fe.edu.dozer.model.game.elements.Boulder;
+import pt.up.fe.edu.dozer.model.game.elements.Target;
+
+import java.util.Arrays;
+
+public class TargetControllerTest {
+    @Test
+    public void update() {
+        Arena arena = Mockito.mock(Arena.class);
+        Mockito.when(arena.getTargets()).thenReturn(Arrays.asList(new Target(1,2), new Target(3,4), new Target(5,6)));
+        Mockito.when(arena.getBoulders()).thenReturn(Arrays.asList(new Boulder(4,4), new Boulder(1,1), new Boulder(3,4)));
+        TargetController controller = new TargetController(arena);
+
+        controller.update();
+
+        Assertions.assertEquals(1, controller.getBouldersInTargets());
+    }
+}
