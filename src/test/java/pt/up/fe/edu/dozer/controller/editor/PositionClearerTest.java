bedd45c0abd64975f410cca1fc6b6e7eec903e49
Onso37 <80353484+Onso37@users.noreply.github.com>
2022-12-22 19:51:49 +0000

    fixed duplicate code

--- src/test/java/pt/up/fe/edu/dozer/controller/editor/PositionClearerTest.java
+++ src/test/java/pt/up/fe/edu/dozer/controller/editor/PositionClearerTest.java
@@ -37,14 +37,33 @@ public class PositionClearerTest {
         Mockito.verify(arena, Mockito.times(1)).getTargets();
     }
 
-    @Test
-    public void testWithArenaWall() {
+    public EditorArena setUpArena(int placerX, int placerY) {
         EditorArena arena = new EditorArena(20, 20);
-        arena.setPlacer(new Placer(15, 5));
+        arena.setPlacer(new Placer(placerX, placerY));
         arena.setDozer(new Dozer(2, 3));
         arena.setBoulders(new ArrayList<>(Arrays.asList(new Boulder(1, 1), new Boulder(6, 7), new Boulder(6, 12))));
         arena.setTargets(new ArrayList<>(Arrays.asList(new Target(2,2), new Target(10,10), new Target(18, 2))));
         arena.setCollisionWalls(new ArrayList<>(Arrays.asList(new ImportantWall(2, 1), new ImportantWall(10, 20), new ImportantWall(15, 5))));
+        return arena;
+    }
+
+    public void assertPositions(EditorArena arena, Position expectedPlacer, Position expectedDozer, List<Position> expectedBoulders, List<Position> expectedTargets, List<Position> expectedWalls) {
+        Assertions.assertEquals(expectedPlacer, arena.getPlacer().getPosition());
+        Assertions.assertEquals(expectedDozer, arena.getDozer().getPosition());
+        for (int i = 0; i < Integer.max(expectedBoulders.size(), arena.getBoulders().size()); i++) {
+            Assertions.assertEquals(expectedBoulders.get(i), arena.getBoulders().get(i).getPosition());
+        }
+        for (int i = 0; i < Integer.max(expectedTargets.size(), arena.getTargets().size()); i++) {
+            Assertions.assertEquals(expectedTargets.get(i), arena.getTargets().get(i).getPosition());
+        }
+        for (int i = 0; i < Integer.max(expectedWalls.size(), arena.getCollisionWalls().size()); i++) {
+            Assertions.assertEquals(expectedWalls.get(i), arena.getCollisionWalls().get(i).getPosition());
+        }
+    }
+
+    @Test
+    public void testWithArenaWall() {
+        EditorArena arena = setUpArena(15, 5);
         Position expectedPlacer = new Position(15, 5);
         Position expectedDozer = new Position(2, 3);
         List<Position> expectedBoulders = Arrays.asList(new Position(1, 1), new Position(6, 7), new Position(6, 12));
@@ -54,27 +73,12 @@ public class PositionClearerTest {
         EditorPositionClearer clearer = new EditorPositionClearer(arena);
         clearer.clearPosition(new Position(15, 5));
 
-        Assertions.assertEquals(expectedPlacer, arena.getPlacer().getPosition());
-        Assertions.assertEquals(expectedDozer, arena.getDozer().getPosition());
-        for (int i = 0; i < Integer.max(expectedBoulders.size(), arena.getBoulders().size()); i++) {
-            Assertions.assertEquals(expectedBoulders.get(i), arena.getBoulders().get(i).getPosition());
-        }
-        for (int i = 0; i < Integer.max(expectedTargets.size(), arena.getTargets().size()); i++) {
-            Assertions.assertEquals(expectedTargets.get(i), arena.getTargets().get(i).getPosition());
-        }
-        for (int i = 0; i < Integer.max(expectedWalls.size(), arena.getCollisionWalls().size()); i++) {
-            Assertions.assertEquals(expectedWalls.get(i), arena.getCollisionWalls().get(i).getPosition());
-        }
+        assertPositions(arena, expectedPlacer, expectedDozer, expectedBoulders, expectedTargets, expectedWalls);
     }
 
     @Test
     public void testWithArenaBoulder() {
-        EditorArena arena = new EditorArena(20, 20);
-        arena.setPlacer(new Placer(1, 1));
-        arena.setDozer(new Dozer(2, 3));
-        arena.setBoulders(new ArrayList<>(Arrays.asList(new Boulder(1, 1), new Boulder(6, 7), new Boulder(6, 12))));
-        arena.setTargets(new ArrayList<>(Arrays.asList(new Target(2,2), new Target(10,10), new Target(18, 2))));
-        arena.setCollisionWalls(new ArrayList<>(Arrays.asList(new ImportantWall(2, 1), new ImportantWall(10, 20), new ImportantWall(15, 5))));
+        EditorArena arena = setUpArena(1, 1);
         Position expectedPlacer = new Position(1, 1);
         Position expectedDozer = new Position(2, 3);
         List<Position> expectedBoulders = Arrays.asList(new Position(6, 7), new Position(6, 12));
@@ -84,27 +88,12 @@ public class PositionClearerTest {
         EditorPositionClearer clearer = new EditorPositionClearer(arena);
         clearer.clearPosition(new Position(1, 1));
 
-        Assertions.assertEquals(expectedPlacer, arena.getPlacer().getPosition());
-        Assertions.assertEquals(expectedDozer, arena.getDozer().getPosition());
-        for (int i = 0; i < Integer.max(expectedBoulders.size(), arena.getBoulders().size()); i++) {
-            Assertions.assertEquals(expectedBoulders.get(i), arena.getBoulders().get(i).getPosition());
-        }
-        for (int i = 0; i < Integer.max(expectedTargets.size(), arena.getTargets().size()); i++) {
-            Assertions.assertEquals(expectedTargets.get(i), arena.getTargets().get(i).getPosition());
-        }
-        for (int i = 0; i < Integer.max(expectedWalls.size(), arena.getCollisionWalls().size()); i++) {
-            Assertions.assertEquals(expectedWalls.get(i), arena.getCollisionWalls().get(i).getPosition());
-        }
+        assertPositions(arena, expectedPlacer, expectedDozer, expectedBoulders, expectedTargets, expectedWalls);
     }
 
     @Test
     public void testWithArenaTarget() {
-        EditorArena arena = new EditorArena(20, 20);
-        arena.setPlacer(new Placer(10, 10));
-        arena.setDozer(new Dozer(2, 3));
-        arena.setBoulders(new ArrayList<>(Arrays.asList(new Boulder(1, 1), new Boulder(6, 7), new Boulder(6, 12))));
-        arena.setTargets(new ArrayList<>(Arrays.asList(new Target(2,2), new Target(10,10), new Target(18, 2))));
-        arena.setCollisionWalls(new ArrayList<>(Arrays.asList(new ImportantWall(2, 1), new ImportantWall(10, 20), new ImportantWall(15, 5))));
+        EditorArena arena = setUpArena(10, 10);
         Position expectedPlacer = new Position(10, 10);
         Position expectedDozer = new Position(2, 3);
         List<Position> expectedBoulders = Arrays.asList(new Position(1, 1), new Position(6, 7), new Position(6, 12));
@@ -114,16 +103,6 @@ public class PositionClearerTest {
         EditorPositionClearer clearer = new EditorPositionClearer(arena);
         clearer.clearPosition(new Position(10, 10));
 
-        Assertions.assertEquals(expectedPlacer, arena.getPlacer().getPosition());
-        Assertions.assertEquals(expectedDozer, arena.getDozer().getPosition());
-        for (int i = 0; i < Integer.max(expectedBoulders.size(), arena.getBoulders().size()); i++) {
-            Assertions.assertEquals(expectedBoulders.get(i), arena.getBoulders().get(i).getPosition());
-        }
-        for (int i = 0; i < Integer.max(expectedTargets.size(), arena.getTargets().size()); i++) {
-            Assertions.assertEquals(expectedTargets.get(i), arena.getTargets().get(i).getPosition());
-        }
-        for (int i = 0; i < Integer.max(expectedWalls.size(), arena.getCollisionWalls().size()); i++) {
-            Assertions.assertEquals(expectedWalls.get(i), arena.getCollisionWalls().get(i).getPosition());
-        }
+        assertPositions(arena, expectedPlacer, expectedDozer, expectedBoulders, expectedTargets, expectedWalls);
     }
 }
