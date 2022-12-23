5bf0705d6be7c0e3454073aa304d02b0eea3f80e
Onso37 <80353484+Onso37@users.noreply.github.com>
2022-12-23 14:57:21 +0000

    add test for PlacerViewerBuilder

---
+++ src/test/java/pt/up/fe/edu/dozer/viewer/PlacerViewerBuilderTest.java
@@ -0,0 +1,15 @@
+package pt.up.fe.edu.dozer.viewer;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pt.up.fe.edu.dozer.viewer.game.PlacerViewerBuilder;
+
+public class PlacerViewerBuilderTest {
+
+    @Test
+    public void checkNotNull() {
+        PlacerViewerBuilder builder = new PlacerViewerBuilder();
+
+        Assertions.assertNotNull(builder.getPlacerViewer());
+    }
+}
